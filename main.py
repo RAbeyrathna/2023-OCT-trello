0490450e6b756426174505783f1a0db4e41b5eec
RAbeyrathna <rahal@res-network.com>
2024-02-08 20:31:20 +1100

    inital setup

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from init import db, ma, bcrypt, jwt
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # Configs
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+    app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+
+    # Connect libraries with flask app
+    db.init_app(app)
+    ma.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    return app
